3246564d7b6917848cd46f92610a86823df2eb14
Daniel0110000 <cariasdaniel261@gmail.com>
2022-12-22 15:55:03 -0600

    Constants

--- app/src/main/java/com/example/newsapp/domain/utilities/Constants.kt
+++ app/src/main/java/com/example/newsapp/domain/utilities/Constants.kt
@@ -5,5 +5,11 @@ object Constants {
     const val API_KEY = "651ff17735bb4c85bed50a5974e81144"
     const val BASE_URL = "https://newsapi.org"
     const val PATH_URL = "/v2/top-headlines"
+    const val BUSINESS = "business"
+    const val ENTERTAINMENT = "entertainment"
+    const val HEALTH = "health"
+    const val SCIENCE = "science"
+    const val SPORTS = "sports"
+    const val TECHNOLOGY = "technology"
 
 }
